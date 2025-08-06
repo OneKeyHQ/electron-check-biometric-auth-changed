177e360b460d76787fee064f7c5534a4d054423d
huhuanming <huanming@onekey.so>
2025-08-06 15:55:04 +0800

    chore: precompile bindings. (#4)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const checkBiometricAuthChanged = () => {
   if (typeof process !== 'undefined' && process.platform && process.platform === 'darwin') {
-    const auth = require('bindings')('auth.node')
+    const auth =
+      process.arch === 'arm64' ? require('./auth-arm64.node') : require('./auth-x64.node')
     return auth.checkBiometricAuthChanged()
   }
   return false
